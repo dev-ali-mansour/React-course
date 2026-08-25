989ad5f6dae9cb00b2ee1f2e29565302ac76ff84
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-25 12:46:31 +0300

    refactor: update App component to improve structure and content

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  // const element = <h1>Hello, world!</h1>;
+  const name = "Alice";
+  const buttonLable = "Click Me";
+  const imageUrl = "https://placehold.co/100x100";
 
   return (
     <div>
-      <h1>Hello, world!</h1>
-      {6 + 7}
-      <p>Hi</p>
+      <h1 className="greeting">Hello, {name}!</h1>
+      <button
+        style={{
+          backgroundColor: "green",
+          color: "white",
+          borderRadius: "5px",
+          margin: "10px",
+        }}
+      >
+        {buttonLable}
+      </button>
+      <button onClick={() => alert("button Clicked!")}>Alert Me!</button>
+
+      <br />
+      <img src={imageUrl}></img>
     </div>
   );
 }
