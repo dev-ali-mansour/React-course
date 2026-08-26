729f833ac4dc06a43b62ea881d2a377249edd53e
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-26 08:41:22 +0300

    feat: restructure App component and add individual sections for Header, About, Projects, Contact, and Footer

---
+++ react-portofilio/src/components/Contact.jsx
@@ -0,0 +1,13 @@
+function Contact(){
+    return (
+        <section id="contact" className="contact-section">
+        <h2>Contact Me</h2>
+        <p>
+          If you would like to get in touch, feel free to email me at{" "}
+          <a href="mailto:john.doe@example.com">john.doe@example.com</a>
+        </p>
+      </section>
+    );
+}
+
+export default Contact;
