6d24742820f7e8724ef6a7622596f73daea8a907
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-26 14:24:20 +0300

    feat: add increment and decrement functions for counter in App component

--- VITE-REACT/src/App.jsx
+++ VITE-REACT/src/App.jsx
@@ -113,11 +113,19 @@ function App() {
 
   const [count, setCount] = useState(0);
 
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+  
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className="app-container">
       <h1>Counter Value:{count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={incrementCount}>Increment</button>
+      <button onClick={decrementCount}>Decrement</button>
     </div>
   );
 }
