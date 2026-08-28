5c11295ab6bd3406fb578f027ea9ff825ff31d3d
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-28 15:08:12 +0300

    feat: add input focus functionality with highlighting and reset option

--- VITE-REACT/src/App.jsx
+++ VITE-REACT/src/App.jsx
@@ -345,7 +345,7 @@ function App() {
     <div >
       <DigitalClock />
     </div>
-  );*/
+  );
 
   const [stateCount, setStateCount] = useState(0);
   const refCount = useRef(0);
@@ -371,6 +371,39 @@ function App() {
       <button onClick={incrementStateCount}>Increment State Count</button>
       <p>Ref Count: {refCount.current}</p>
       <button onClick={incrementRefCount}>Increment Ref Count</button>
+    </div>
+  );*/
+
+  const inputRef = useRef(null);
+  const inputRefNext = useRef(null);
+
+  useEffect(() => {
+    console.log("Component Re-rendered");
+  });
+
+  const focusInput = () => {
+    inputRef.current.focus();
+    inputRef.current.style.backgroundColor = "yellow";
+  };
+
+  const focusInputNext = () => {
+    inputRefNext.current.focus();
+    inputRefNext.current.style.backgroundColor = "yellow";
+  };
+
+  const resetFocus = () => {
+    inputRef.current.style.backgroundColor = "white";
+    inputRefNext.current.style.backgroundColor = "white";
+  };
+
+  return (
+    <div>
+      <h1>Learn React</h1>
+      <input ref={inputRef} type="text" placeholder="Focus me"></input>
+      <button onClick={focusInput}>Focus and Highlight</button>
+      <input ref={inputRefNext} type="text" placeholder="Focus me"></input>
+      <button onClick={focusInputNext}>Focus and Highlight</button>
+      <button onClick={resetFocus}>Reset</button>
     </div>
   );
 }
