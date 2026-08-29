f0439ec6fb005c8abba2153314db275681fba856
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-29 11:27:31 +0300

    feat: implement routing with Home, About, and Contact components

--- routing-react/src/App.jsx
+++ routing-react/src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+
+/*
+  / -> Home
+  /about -> About
+  /contact -> Contact
+*/
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
