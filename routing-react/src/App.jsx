57a4df1cb0b5fdd6854cabc8bfc6b801014ea10b
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-29 12:35:26 +0300

    feat: add CurrentLocation component to display the current path

--- routing-react/src/App.jsx
+++ routing-react/src/App.jsx
@@ -5,6 +5,7 @@ import About from "./About";
 import Contact from "./Contact";
 import Team from "./Team";
 import NotFound from "./NotFound";
+import CurrentLocation from "./CurrentLocation";
 
 /*
   / -> Home
@@ -45,6 +46,7 @@ function App() {
           </ul>
         </nav>
 
+        <CurrentLocation />
         <div className="container mx-auto py-8">
           <Routes>
             <Route path="/" element={<Home />} />
