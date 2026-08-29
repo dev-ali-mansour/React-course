bbf0e451a77e30310e4bc84e22fcf6776c38be44
Ali Mansour <dev.ali.mansour@gmail.com>
2026-08-29 16:32:08 +0300

    feat: implement Redux for fetching and displaying posts

---
+++ redux-project/src/store/actions/postActions.js
@@ -0,0 +1,25 @@
+export const fetchPostsRequest = () => ({
+  type: "FETCH_POSTS_REQUEST",
+});
+export const fetchPostsSuccess = (posts) => ({
+  type: "FETCH_POSTS_SUCCESS",
+  payload: posts,
+});
+export const fetchPostsFailure = (error) => ({
+  type: "FETCH_POSTS_FAILURE",
+  payload: error,
+});
+
+export const fetchPosts = () => async (dispatch) => {
+  // Dispatch the fetch posts
+  dispatch(fetchPostsRequest());
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+    // Dispatch the fetch success
+    dispatch(fetchPostsSuccess(data));
+  } catch (error) {
+    // Dispatch the fetch failure
+    dispatch(fetchPostsFailure(error.message));
+  }
+};
